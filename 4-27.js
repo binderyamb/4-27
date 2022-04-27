eb70b455ce4a4abb4ecc76b4257922c32e2e3c1b
binderyamb <79251475+binderyamb@users.noreply.github.com>
2022-04-27 19:51:15 +0800

    Add files via upload

---
+++ 4-27.js
@@ -0,0 +1,60 @@
+//dawtalt
+//for loop parametrtei dawtalt
+
+// for(var i = 1; i <= 20 ; i++){
+//     console.log(i);
+// }
+
+// for(var  i = 0; i <= 100; i=i++){
+//     if(i%3==0 && i%7==0)console.log(i);
+// }
+
+
+// for(var i = 1; i <= 100; i=i+2){
+//     console.log(i);
+// }
+
+// var s = 0;
+// for(var i = 1; i <= 10; i++){
+//     s = s + i;
+// }
+// console.log(s);
+
+// var s = 1;
+// for (var i = 1; i <=20; i=i+2){
+//     s = s * i;
+// }
+// console.log(s);
+
+//while loop-nuhtsult dawtalt
+
+// var n = 9876;
+// while(n > 10){
+//     n = parseInt(n/10);
+// }
+// console.log(n);
+
+// var n = (123);
+// var s = 0;
+// while(n > 0 ){
+//     s = s + (n%10);
+//     n = parseInt(n/10);
+// }
+// console.log(s);
+
+// var n = prompt();
+// max = n%10;
+// n = parseInt(n/10);
+// while(n > 0){
+//     a = n%10;
+//     if( a > max) max = a;
+//     n = n/10;
+// }
+// alert(max);
+
+// var n = prompt();
+// while(n > 0){
+//     m = n % 10;
+//     if( m > 5) alert(m);
+//     n = parseInt(n/10);
+// }
